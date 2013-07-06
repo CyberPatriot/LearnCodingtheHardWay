d86874b625e6b98081d7871515985b5b0e0b3548
phrasz <dev@null.com>
2013-07-05 20:05:09 -0400

    Addded Ex10 on LPTHW

---
+++ Python/Ex01/ex1_windows_phrasz.py
@@ -0,0 +1,16 @@
+# CAUTION!
+#
+#	Python 3.0 Users:
+#	-----------------
+#
+#	Print is now a FUNCTION!
+#
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print("I \"said\" do not touch this.")
+
+
