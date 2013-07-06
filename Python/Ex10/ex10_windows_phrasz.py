75dd5e64825a4e93ff5e1d77229bb8f76d372611
phrasz <dev@null.com>
2013-07-05 20:15:14 -0400

    Removed silly system calls; Placed two different ways to CR print the "loading" bar

--- Python/Ex10/ex10_windows_phrasz.py
+++ Python/Ex10/ex10_windows_phrasz.py
@@ -1,6 +1,3 @@
-#from subprocess import call #ADDED for system call # Try 1
-import os #Added for System Call #Try2
-
 tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
@@ -17,15 +14,7 @@ print(persian_cat)
 print(backslash_cat)
 print(fat_cat)
 
-
-#while True:
-#	for i in ["/","-","|","\\","|"]:
-#		print ("%s\r" % i,)
-
-		#>>> clear = lambda: os.system('cls')
-		#>>> clear()
-
-        #print ("%s\r"); #this line DOESN'T PRINT ANYTHING
-		
-		
-		#call(["ls", "-l"]) #TRY1
+while True:
+	for i in ["/","-","|","\\","|"]:
+		print ("%s\r" % i,end="")
+		#print (i,"\r",end="") SAME DIFF
